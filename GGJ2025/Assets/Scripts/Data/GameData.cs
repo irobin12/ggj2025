87a99827951df3b2ae2ba1f7f5d09b986b7704eb
IRobin <inesrobin@pm.me>
2025-01-25 22:28:31 +0000

    Removed unused property for screen width for launch

--- GGJ2025/Assets/Scripts/Data/GameData.cs
+++ GGJ2025/Assets/Scripts/Data/GameData.cs
@@ -6,12 +6,6 @@ public class GameData : ScriptableObject
     [Range(0, 90)]
     [SerializeField] private int maxLaunchAngle = 45;
     public int MaxLaunchAngle => maxLaunchAngle;
-
-    [Range(0, 1f)]
-    // [Tooltip("0 = edge of the screen is max angle, 0.5 = middle of the screen is max angle (not possible to change it)")]
-    [Tooltip("0 = no width considered, angle cannot be changed, 1 = full screen width is considered")]
-    [SerializeField] private float screenProportionForLaunchAngle = 0.8f;
-    public float ScreenProportionForLaunchAngle => screenProportionForLaunchAngle;
     
     [Range(10, 500)]
     [SerializeField] private int maxLaunchImpulse = 50;
