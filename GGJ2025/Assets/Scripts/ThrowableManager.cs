1aaec2827fe9c88ebdb9181b75a932885c007e74
IRobin <inesrobin@pm.me>
2025-01-25 21:57:03 +0000

    Fixed and simplified camera, simple move forward and down now

--- GGJ2025/Assets/Scripts/ThrowableManager.cs
+++ GGJ2025/Assets/Scripts/ThrowableManager.cs
@@ -13,20 +13,14 @@ public class ThrowableManager : MonoBehaviour
     private Quaternion initialRotation;
     
     private Vector3 cameraInitialPosition;
-    private Vector3 cameraStartPosition;
     private Quaternion cameraInitialRotation;
+    private Vector3 cameraDeltaPositionFromThrowable;
     
     private Vector3 throwableInitialPosition;
     private Quaternion throwableInitialRotation;
     
     private float minLaunchAngle;
     private float maxLaunchAngle;
-    // private float minScreenMarginForLaunchAngle;
-    // private float maxScreenMarginForLaunchAngle;
-    // private float screenProportionForLaunchAngle;
-    private float screenWidthToConsider;
-    private float mouseOffset;
-    
     
     private int maxLaunchImpulse;
 
@@ -40,14 +34,11 @@ public class ThrowableManager : MonoBehaviour
         
         throwableInitialPosition = throwable.transform.position;
         throwableInitialRotation = throwable.transform.rotation;
+
+        cameraDeltaPositionFromThrowable = throwableInitialPosition - cameraInitialPosition;
         
         minLaunchAngle = throwableInitialRotation.eulerAngles.y - dataMaxLaunchAngle;
         maxLaunchAngle = throwableInitialRotation.eulerAngles.y + dataMaxLaunchAngle;
-        // minScreenMarginForLaunchAngle = 0 + dataScreenProportionForLaunchAngle;
-        // maxScreenMarginForLaunchAngle = 1 - dataScreenProportionForLaunchAngle;
-        // screenProportionForLaunchAngle = dataScreenProportionForLaunchAngle;
-        screenWidthToConsider = Screen.width * dataScreenProportionForLaunchAngle;
-        mouseOffset = (Screen.width - screenWidthToConsider) / 2;
         
         maxLaunchImpulse = dataMaxLaunchImpulse;
         
@@ -74,7 +65,7 @@ public class ThrowableManager : MonoBehaviour
         {
             case GameStatesManager.GameState.Launch:
             {
-                UpdateLauncherRotation();
+                UpdateLaunchingRotation();
                 if (Input.GetMouseButtonDown(0))
                 {
                     LaunchThrowable();
@@ -98,25 +89,17 @@ public class ThrowableManager : MonoBehaviour
 
     private void LaunchThrowable()
     {
-        cameraStartPosition = followerCamera.transform.position;
         throwable.Launch(maxLaunchImpulse);
         ShowLauncherVisuals(false);
         GameStatesManager.SetGameState(GameStatesManager.GameState.Rolling);
     }
 
-    private void UpdateLauncherRotation()
+    private void UpdateLaunchingRotation()
     {
         var mousePosition = Input.mousePosition;
-        // var mouseX = Mathf.Clamp(mousePosition.x, mouseOffset, Screen.width - mouseOffset);
-        // var newAngle = Mathf.LerpAngle(minLaunchAngle, maxLaunchAngle, mouseX / screenWidthToConsider);
         var newAngle = Mathf.LerpAngle(minLaunchAngle, maxLaunchAngle, mousePosition.x / Screen.width);
-        // followerCamera.transform.rotation = new Vector3(cameraInitialRotation.x, newAngle, cameraInitialRotation.z);
-        var newRotation = Quaternion.Euler(0, newAngle, 0);
-        transform.rotation = newRotation;
-        // var newRotation = Quaternion.Euler(cameraInitialRotation.eulerAngles.x, newAngle, cameraInitialRotation.eulerAngles.z);
-        // followerCamera.transform.rotation = newRotation;
-        // launcherVisual.transform.rotation = newRotation;
-        // throwable.transform.rotation = newRotation;
+        launcherVisual.transform.rotation = Quaternion.Euler(launcherVisual.transform.rotation.eulerAngles.x, newAngle, 0);
+        throwable.transform.rotation = Quaternion.Euler(throwable.transform.rotation.eulerAngles.x, newAngle, throwable.transform.rotation.eulerAngles.z);
     }
 
     private void FixedUpdate()
@@ -129,41 +112,8 @@ public class ThrowableManager : MonoBehaviour
 
     private void UpdateCameraTransformFromThrowable()
     {
-        Vector3 positionOffset = throwable.transform.position - throwableInitialPosition;
-        followerCamera.transform.position = cameraStartPosition + positionOffset;
-        
-        // var direction = throwable.rigidBody.
-        // var velocity = throwable.rigidBody.GetPointVelocity(Vector3.zero);
-        // Debug.Log(velocity);
-        // var facing = throwable.rigidBody.rotation * Vector3.forward;
-        var velocity = throwable.rigidBody.linearVelocity;
-        // float angleDifference = Vector3.Angle(facing, velocity);
-        // float relativeAngleDifference = Vector3.SignedAngle(facing, velocity, Vector3.up);
-        // followerCamera.transform.rotation = Quaternion.AngleAxis(10f, Vector3.up);
-        var currentYRotation = GetCameraYRotation();
-        followerCamera.transform.rotation = Quaternion.LookRotation(velocity, Vector3.up);
-        var newYRotation = GetCameraYRotation();
-        if (!Mathf.Approximately(newYRotation, currentYRotation))
-        {
-            // StartCoroutine(RotateCamera(currentYRotation, newYRotation));
-            followerCamera.transform.rotation = Quaternion.Euler(new Vector3(cameraInitialRotation.eulerAngles.x, newYRotation, cameraInitialRotation.eulerAngles.z));
-        }
-    }
-
-    private IEnumerator RotateCamera(float originYRotation, float targetYRotation)
-    {
-        float currentYRotation = originYRotation;
-        while (!Mathf.Approximately(currentYRotation, targetYRotation))
-        {
-            currentYRotation += Time.deltaTime * 10f;
-            followerCamera.transform.rotation = Quaternion.Euler(0, currentYRotation, 0);
-            yield return null;
-        }
-    }
-
-    private float GetCameraYRotation()
-    {
-        return followerCamera.transform.rotation.eulerAngles.y;
+        // x should stay fixed (width), z is length.depth, y is height
+        followerCamera.transform.position = new Vector3(followerCamera.transform.position.x, throwable.transform.position.y - cameraDeltaPositionFromThrowable.y, throwable.transform.position.z - cameraDeltaPositionFromThrowable.z);
     }
 
     private void ShowLauncherVisuals(bool show)
