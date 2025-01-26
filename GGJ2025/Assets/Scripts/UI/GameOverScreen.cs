59de1f671528dc38a7eb167d3460a404e8abf0e4
IRobin <inesrobin@pm.me>
2025-01-26 18:55:49 +0000

    Added level name in Game Over screen

--- GGJ2025/Assets/Scripts/UI/GameOverScreen.cs
+++ GGJ2025/Assets/Scripts/UI/GameOverScreen.cs
@@ -1,17 +1,20 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameOverScreen : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI scoreText;
     [SerializeField] private TextMeshProUGUI saveText;
+    [SerializeField] private TextMeshProUGUI levelText;
     [SerializeField] private Button menuButton;
 
     public void SetTexts(int score, int save, int maxSaves)
     {
         scoreText.text = $"Final score: {score}";
         saveText.text = $"You saved: {save}/{maxSaves}";
+        levelText.text = $"Current level: {LevelsManager.currentLevelName}";
     }
     
     public void HandleMenuButton()
