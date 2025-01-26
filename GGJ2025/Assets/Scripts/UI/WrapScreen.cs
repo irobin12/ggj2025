34f97d68fac1a067b5e4831774c7ef20203201be
IRobin <inesrobin@pm.me>
2025-01-26 13:06:00 +0000

    Wrap screen UI

--- GGJ2025/Assets/Scripts/UI/WrapScreen.cs
+++ GGJ2025/Assets/Scripts/UI/WrapScreen.cs
@@ -1,9 +1,18 @@
 using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WrapScreen : MonoBehaviour
 {
     public Action StartClicked;
+    
+    [SerializeField] private TextMeshProUGUI nameText;
+    [SerializeField] private TextMeshProUGUI healthText;
+    [SerializeField] private TextMeshProUGUI wrapRemainingText;
+    [SerializeField] private TextMeshProUGUI wrapAssignedNumberText;
+    [SerializeField] private TextMeshProUGUI assignAllText;
+    [SerializeField] private Button startButton;
     
     public void HandleStartClicked()
     {
